825c04e5dfed80bb7760d96664e1f4dcdf94cdbc
mat-hud <41866940+mat-hud@users.noreply.github.com>
2018-08-07 16:25:55 +0100

    Add files via upload

--- Overtime.py
+++ Overtime.py
@@ -3,8 +3,8 @@ from tkinter import font  as tkfont
 
 import matplotlib as plt
 from matplotlib import style
-import datetime
-from datetime import datetime
+import datetime 
+from datetime import timedelta
 
        
 class OvertimeApp(tk.Tk):
@@ -41,9 +41,9 @@ class HomePage(tk.Frame):
         
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        label = tk.Label(self, text='This is the home page', font=controller.title_font)
+        label = tk.Label(self, text='Home Page', font=controller.title_font)
         label.grid(row=1, column=2)
-        self.time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        self.time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         self.timelabel = tk.Label(self, text=self.time, font=controller.sub_title_font)
         self.timelabel.grid(row=2, column=2)
         self.update_time()
@@ -58,7 +58,6 @@ class HomePage(tk.Frame):
         button3 = tk.Button(self, text='Settings',
                              command = lambda: controller.show_frame('Settings'))    
                               
-        
         cibutton = tk.Button(self, text='Check In',
                              command = self.check_in_time)
         cobutton = tk.Button(self, text='Check Out',
@@ -72,6 +71,9 @@ class HomePage(tk.Frame):
         self.cotime_text = ''
         self.colabel = tk.Label(self, text= self.cotime_text)
         self.colabel.grid(row=4, column=2)
+        self.ottext = ''      
+        self.otlabel = tk.Label(self, text = self.ottext)
+        self.otlabel.grid(row=5, column=2)
         
         cibutton.grid(row=3, column=1,columnspan=2,sticky='W',padx=1,pady=1)                      
         cobutton.grid(row=4, column=1,columnspan=2,sticky='W',padx=1,pady=1)
@@ -79,26 +81,24 @@ class HomePage(tk.Frame):
         button1.grid(row=10, column=1,sticky='SW',padx=2,pady=2)
         button2.grid(row=10, column=2,sticky='S',padx=2,pady=2)
         button3.grid(row=10, column=3,sticky='SE',padx=2,pady=2)
-        
-         
+             
     def update_time(self):
-        timenow = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        timenow = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         self.time = timenow
         self.timelabel.config(text=self.time)
         self.timelabel.after(200, self.update_time)  
-      
-
+     
     def check_in_time(self):
         
         if self.onsite == False:
             self.onsite = True
-            self.citime = datetime.now()
-            self.citime_text = datetime.now().strftime(' %H:%M ')
+            self.citime = datetime.datetime.now()
+            self.citime_text = datetime.datetime.now().strftime(' %H:%M ')
             self.update_label()
-    
+
     def check_out_time(self):
         if self.onsite == True:
-            self.cotime = datetime.now()
+            self.cotime = datetime.datetime.now()
             self.cotime_text = self.cotime.strftime(' %H:%M ')
             self.update_label()
             self.overtime_calc()
@@ -107,24 +107,49 @@ class HomePage(tk.Frame):
         self.onsite = False
         self.cotime_text = ''
         self.citime_text = ''
+        self.ottext = ''
         self.update_label()  
         
     def update_label(self):
        self.cilabel.config(text=self.citime_text)
        self.colabel.config(text=self.cotime_text)
+       self.otlabel.config(text=self.ottext)
     
     def overtime_calc(self):
      
-       timein = self.cotime - self.citime
-       overtime = timein
-       print(overtime)
-        
-        
-
-
+       inputshiftpatten = 'Enginnering Day Shift'
+       #dayoftheweek = datetime.datetime.today().weekday()
+       dayoftheweek = 5
+       shift_patten = {'Enginnering Day Shift':[[7,55,16,40,30,1,1.5],[7,55,16,40,30,1,1.5],[7,55,16,40,30,1,1.5],[7,55,16,40,30,1,1.5],[7,55,14,25,30,1,1.5],[0,0,0,0,0,1,1.5],[0,0,0,0,0,1,2]]}
        
-
-
+       contracthours = shift_patten[inputshiftpatten]
+      
+       stimehours = contracthours[dayoftheweek][0]
+       stimemins = contracthours[dayoftheweek][1]
+       ftimehours = contracthours[dayoftheweek][2]
+       ftimemins = contracthours[dayoftheweek][3]
+       ltimemins = contracthours[dayoftheweek][4]
+       shiftmulti = contracthours[dayoftheweek][5]
+       otmulti = contracthours[dayoftheweek][6]
+      
+       stime = timedelta(hours=stimehours, minutes=stimemins)
+       ftime = timedelta(hours=ftimehours, minutes=ftimemins)
+       ltime = timedelta(minutes=ltimemins)
+       conthours = ftime - stime - ltime
+       hoursin = self.cotime - self.citime
+      
+       if hoursin > conthours:
+           overtime = hoursin - conthours
+           overtime_text = ':'.join(str(overtime).split(':')[:2])
+           
+       else: 
+           overtime = timedelta(hours=0)
+           overtime_text='0:00'
+           
+       self.ottext = "Overtime: " + str(overtime_text)    
+       self.update_label()  
+   
+   
 class PageOne(tk.Frame):
     
     def __init__(self, parent, controller):
@@ -152,7 +177,6 @@ class PageTwo(tk.Frame):
 
 
 class Settings(tk.Frame):
-    
  
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -176,8 +200,6 @@ class Settings(tk.Frame):
         e4label = tk.Label(self, text='Start Time')
         e4label.grid(row=4,column=3)
         
-       
-        
         e1 = tk.Entry(self, width=10)
         e1.grid(row=3, column=2)
         e2 = tk.Entry(self, width=10)
@@ -195,18 +217,6 @@ class Settings(tk.Frame):
         e8 = tk.Entry(self, width=10)
         e8.grid(row=6, column=4)
         
-       
-        #var = tk.StringVar()
-        #var.set(self.shiftpatten[0])
-        #self.dd = tk.OptionMenu(Settings(), var, self.shiftpatten)
-        #ddlabel.grid(row=7,column=2)
-        
-        
-    def click(self):
-        entered_text = entry.get()
-        
-    
-       
 if __name__ == '__main__':
     app = OvertimeApp()
     app.mainloop()
